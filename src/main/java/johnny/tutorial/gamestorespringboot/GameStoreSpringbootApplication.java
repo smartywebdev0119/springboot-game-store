f92950d6c42fe234a446ffa3df965caee0b58883
smartywebdev0119 <smartwebdev0119@gmail.com>
2022-01-10 15:41:18 -0800

    create gradle project with https://start.spring.io/

---
+++ src/main/java/johnny/tutorial/gamestorespringboot/GameStoreSpringbootApplication.java
@@ -0,0 +1,13 @@
+package johnny.tutorial.gamestorespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/johnny/tutorial/gamestorespringboot/GameStoreSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package johnny.tutorial.gamestorespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
