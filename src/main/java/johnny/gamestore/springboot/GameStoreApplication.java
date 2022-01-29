54440414f1af195cf5bb4aedce0846cf563734a8
smartywebdev0119 <smartwebdev0119@gmail.com>
2022-01-29 11:25:39 -0800

    API Documentation with OpenAPI and Swagger (#9)
    * add openapi dependency
    
    * add swagger annotations to methods
    
    * refine doc description

--- src/main/java/johnny/gamestore/springboot/GameStoreApplication.java
+++ src/main/java/johnny/gamestore/springboot/GameStoreApplication.java
@@ -1,11 +1,11 @@
 package johnny.gamestore.springboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main entry of the application.
- */
+@OpenAPIDefinition(info = @Info(title = "Game Store API", version = "1.0", description = "Product Information"))
 @SpringBootApplication
 public class GameStoreApplication {
   public static void main(String[] args) {
--- src/main/java/johnny/gamestore/springboot/controller/ProductController.java
+++ src/main/java/johnny/gamestore/springboot/controller/ProductController.java
@@ -2,8 +2,15 @@ package johnny.gamestore.springboot.controller;
 
 import johnny.gamestore.springboot.domain.Product;
 import johnny.gamestore.springboot.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +31,12 @@ public class ProductController extends BaseController {
   ProductService productService;
 
   // GET /products
-  @GetMapping("")
+  @Operation(summary = "Get all products", description = "Get all products sorted by id",
+      tags = { "Product Controller" })
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successful retrieved products",
+        content = @Content(array = @ArraySchema(schema = @Schema(implementation = Product.class)))) })
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Product> findAll() {
     List<Product> products = productService.findAll();
     products.forEach(product -> {
@@ -35,6 +47,12 @@ public class ProductController extends BaseController {
   }
 
   // GET /products/5
+  @Operation(summary = "Get one product", description = "Get one product by id", tags = { "Product Controller" })
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successful retrieved one product by id",
+        content = {@Content(schema = @Schema(implementation = Product.class))}),
+      @ApiResponse(responseCode = "404", description = "Product is not found with the given id",
+        content = @Content)})
   @GetMapping("/{id}")
   public ResponseEntity<Product> findOne(@PathVariable(value = "id") long id) throws Exception {
     if (!productService.exists(id)) {
@@ -46,6 +64,10 @@ public class ProductController extends BaseController {
   }
 
   // POST /products
+  @Operation(summary = "Create new product", description = "Create new product", tags = { "Product Controller" })
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Created one new product",
+        content = {@Content(schema = @Schema(implementation = Product.class))})})
   @PostMapping("")
   public ResponseEntity<Product> create(@Valid @RequestBody Product product) {
     product.setImage(product.getImage().replace(getBaseUrl(), ""));
@@ -55,6 +77,12 @@ public class ProductController extends BaseController {
   }
 
   // PUT /products/5
+  @Operation(summary = "Update one product", description = "Update one product", tags = { "Product Controller" })
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Update one product by id",
+        content = {@Content(schema = @Schema(implementation = Product.class))}),
+      @ApiResponse(responseCode = "404", description = "Product is not found with the given id",
+        content = @Content)})
   @PutMapping("/{id}")
   public ResponseEntity<Product> update(@PathVariable(value = "id") Long id,
                                         @Valid @RequestBody Product product) throws Exception {
@@ -72,6 +100,12 @@ public class ProductController extends BaseController {
 
   // DELETE /products/5
   @DeleteMapping("/{id}")
+  @Operation(summary = "Delete one product", description = "Delete one product by id", tags = { "Product Controller" })
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successfully deleted product",
+        content = {@Content(schema = @Schema(implementation = Product.class))}),
+      @ApiResponse(responseCode = "404", description = "Product is not found with the given id",
+        content = @Content)})
   public ResponseEntity<Product> delete(@PathVariable(value = "id") long id) {
     if (!productService.exists(id)) {
       return ResponseEntity.notFound().build();
